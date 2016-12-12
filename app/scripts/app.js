99fe3feb0a6032c7b62c8dbd94e05a6f01d226a3
Tim Hostetler <tim.hostetler@intermedix.com>
2016-12-12 18:44:00 -0500

    Move away from WP

---
+++ app/scripts/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name initApp
+ * @description
+ * # initApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('app', [])
+  .controller('mainController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('/sweetlycbc/data/data.json').then(function (res) {
+      var items = [
+        'menuItems',
+        'flavors',
+        'faqs',
+        'prices',
+        'servingSizes'
+      ];
+      items.forEach(function (i) {
+        $scope[i] = res.data[i];
+      });
+    });
+  }]
+);
